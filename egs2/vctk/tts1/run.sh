d35baf8960d44ed757a4f7a407ec0e3883acace5
chris.grech <christopher.grech@gmail.com>
2024-10-10 16:16:01 +0200

    Updates run.sh to suit it better for VITS training

--- egs2/vctk/tts1/run.sh
+++ egs2/vctk/tts1/run.sh
@@ -5,11 +5,13 @@ set -e
 set -u
 set -o pipefail
 
+# Parameters for audio processing
 fs=22050
 n_fft=1024
 n_shift=256
 win_length=1024
 
+# Determine audio format
 opts=
 if [ "${fs}" -eq 22050 ]; then
     # To suppress recreation, specify wav format
@@ -18,15 +20,15 @@ else
     opts="--audio_format flac "
 fi
 
+# Dataset and model configurations
 train_set=tr_no_dev
 valid_set=dev
 test_sets="dev eval1"
+g2p=none
 
-train_config=conf/train.yaml
-inference_config=conf/decode.yaml
-
-# g2p=g2p_en # Include word separator
-g2p=g2p_en_no_space # Include no word separator
+# Model configuration
+train_config=conf/tuning/train_xvector_vits.yaml
+inference_config=conf/tuning/decode_vits.yaml
 
 ./tts.sh \
     --lang en \
@@ -38,6 +40,10 @@ g2p=g2p_en_no_space # Include no word separator
     --token_type phn \
     --cleaner maltese \
     --g2p "${g2p}" \
+    --tts_task gan_tts \
+    --use_spk_embed true \
+    --spk_embed_tool kaldi \
+    --spk_embed_tag xvector \
     --train_config "${train_config}" \
     --inference_config "${inference_config}" \
     --train_set "${train_set}" \
